42e010120dd6d494d24b54768b3d409640704f38
admin <admin@hackit.me>
2019-11-09 00:30:37 +0800

    测试dnsrebind，设置TTL为0

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 var named = require('./lib/index');
 var server = named.createServer();
 var ttl = 300;
+var rebind = {};
 
 server.listen(53, '127.0.0.1', function() {
-  console.log('DNS server started on port 53');
+	console.log('DNS server started on port 53');
 });
 
 server.on('query', function(query) {
-  var domain = query.name();
-  console.log('DNS Query: %s', domain)
-  var record = new named.ARecord('127.0.0.1');
-  query.addAnswer(domain, record, ttl);
-  server.send(query);
+	var domain = query.name();
+	console.log('DNS Query: %s', domain)
+	if(domain == 'rebind.test.com'){
+		if(typeof(rebind.times) == 'undefined'){
+			rebind.times = 1;
+			var record = new named.ARecord('127.0.0.1');
+			query.addAnswer(domain, record, 0);
+			server.send(query);
+		}else{
+			var record = new named.ARecord('8.8.8.8');
+			query.addAnswer(domain, record, 0);
+			server.send(query);
+			rebind.times ++;
+		}
+
+	}else{
+		var record = new named.ARecord('127.0.0.1');
+		query.addAnswer(domain, record, ttl);
+		server.send(query);		
+	}
 });
